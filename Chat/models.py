720bb07ef24fa4c1db720d6678825a2148dff6fa
RussianGun <34107490+RussianGun@users.noreply.github.com>
2018-02-20 23:56:15 +0300

    .

--- Chat/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class Chat(models.Model):
-    title = models.CharField(max_length=200)
--- Event/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class Event(models.Model):
- title = models.CharField(max_length=200)
- text = models.TextField()
