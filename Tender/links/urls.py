449cbbe2c947a2a5e1f99e553a4f3ab6b65794d7
TihonNarushev <Tihon12345678902mail.ru>
2018-02-21 18:23:09 +0300

    rgdsg

--- Tender/links/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-url(r'^open_link$', views.open_link, name='open _link')
-url(r'^delite_link$', views.delite_link, name='delite _link')
-url(r'^edit_links$', views.edit_links, name='edit_links')
--- Tender/tasks/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class Task(models.Model):
-    task_title = models.CharField(max_length=200)
-    task_text = models.TextField()
-    eventID = models.ForeignKey('events.Event', related_name='tasks',on_delete=models.CASCADE)
-    date = models.DateTimeField(
-        default=timezone.now)
